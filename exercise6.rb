50b9896c46013dc09eb1fe979e9352ee427ac53f
Franky Chau <houun.chau@gmail.com>
2016-01-13 15:01:55 -0500

    Check if need to buy bananas

--- exercise6.rb
+++ exercise6.rb
@@ -7,6 +7,8 @@ def listing(list)
   list.each {|item|puts "* #{item}"}
   # count and output the number of items in list
   puts "Total number of items: #{list.count}"
+  puts list.include?("bananas") ? "You need to pick up bananas" : "You don't need to pick up bananas today."
+  
 end
 
 listing(grocery_list)
