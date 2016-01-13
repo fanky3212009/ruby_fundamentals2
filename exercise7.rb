e72b60ae4bd1d5e6b13eb70dadc0ab78470f3574
Franky Chau <houun.chau@gmail.com>
2016-01-13 16:08:26 -0500

    Add cohort4, display all cohort names, expand by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -6,6 +6,18 @@ students = {
 
 def display(students)
   students.each {|name, number| puts "#{name}: #{number} students"}
+  print "\n"
 end
+
+display(students)
+
+# add cohort 4
+students[:cohort4] = 43
+
+# output all cohort names
+puts students.keys
+
+# expanded!
+students.each {|k,v| students[k] = v * 1.05}
 
 display(students)
