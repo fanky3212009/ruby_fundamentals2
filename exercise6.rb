f6a56489dba510d36c511de71f819d0b56e75f4d
Franky Chau <houun.chau@gmail.com>
2016-01-13 14:59:03 -0500

    Add rice to the list, write method for output, count and output total number of item in list

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,12 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-grocery_list.each {|grocery_item|puts "* #{grocery_item}"}
+# add rice to grocery_list
+grocery_list << "rice"
+
+# method for outputting the list
+def listing(list)
+  list.each {|item|puts "* #{item}"}
+  # count and output the number of items in list
+  puts "Total number of items: #{list.count}"
+end
+
+listing(grocery_list)
