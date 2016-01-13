e6faf44cc28a41b966835f410626c65a4ab80860
Franky Chau <houun.chau@gmail.com>
2016-01-13 13:58:31 -0500

    Add exercise5, with the conversion method, prompts, and outputs

---
+++ exercise5.rb
@@ -0,0 +1,6 @@
+def convert_temp(fahrenheit)
+  (fahrenheit - 32) * 5/9
+end
+puts "Input a temperature in Fahrenheit:"
+fahrenheit = gets.chomp.to_i
+puts "The temperature in Celcius is #{convert_temp(fahrenheit)}.)
